e2f8d5399024a341c6b074e73197cdb16fb1c86d
chauhanrakshit242-sys <chauhanrakshit242@gmail.com>
2026-07-07 18:31:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (62.58%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.cpp
+++ 457-circular-array-loop/circular-array-loop.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
-    int calu(vector<int>& nums, int curr)
+    int nextIndex(vector<int>& nums, bool isPos, int curr)
     {
+        bool dir = nums[curr] > 0;
+
+        if(dir != isPos)
+            return -1;
+
         int n = nums.size();
-        return ((curr + nums[curr]) % n + n) % n;
+        int next = ((curr + nums[curr]) % n + n) % n;
+
+        if(next == curr)
+            return -1;
+
+        return next;
     }
 
     bool circularArrayLoop(vector<int>& nums) {
@@ -12,53 +22,38 @@ public:
 
         for(int i = 0; i < n; i++)
         {
-            set<int> st;
+            if(nums[i] == 0)
+                continue;
 
             bool isPos = nums[i] > 0;
 
-            int curr = i;
+            int slow = i;
+            int fast = i;
 
             while(true)
             {
-                int next = calu(nums, curr);
-
-                // Single element loop is not allowed
-                if(next == curr)
+                slow = nextIndex(nums, isPos, slow);
+                if(slow == -1)
                     break;
 
-                if(isPos)
-                {
-                    if(nums[next] < 0)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        if(st.find(next) != st.end())
-                        {
-                            return true;
-                        }
+                fast = nextIndex(nums, isPos, fast);
+                if(fast == -1)
+                    break;
 
-                        st.insert(next);
-                    }
-                }
-                else
-                {
-                    if(nums[next] > 0)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        if(st.find(next) != st.end())
-                        {
-                            return true;
-                        }
+                fast = nextIndex(nums, isPos, fast);
+                if(fast == -1)
+                    break;
 
-                        st.insert(next);
-                    }
-                }
+                if(slow == fast)
+                    return true;
+            }
 
+            int curr = i;
+
+            while(nums[curr] != 0 && (nums[curr] > 0) == isPos)
+            {
+                int next = ((curr + nums[curr]) % n + n) % n;
+                nums[curr] = 0;
                 curr = next;
             }
         }
